d6084c97cc278bac17f241c190111ff11b187fa8
Rafli Muadzan <raflimuadzan2@gmail.com>
2023-02-08 12:15:23 +0800

    add crud operations to todo

--- dist/controllers/todoController.js
+++ dist/controllers/todoController.js
@@ -8,97 +8,88 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteSingleTodoHandler = exports.updateSingleTodoHandler = exports.getSingleTodoHandler = exports.craeteTodoHandler = exports.getAllTodosHandler = void 0;
+const connect_1 = __importDefault(require("../db/connect"));
 const getAllTodosHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield connect_1.default
+        .selectFrom('todo')
+        .selectAll()
+        .orderBy('id')
+        .execute();
     reply
         .code(200)
         .header('Content-Type', 'application/json; charset=utf-8')
         .send({
         status: "success",
-        result: 2,
-        data: [
-            {
-                id: 1,
-                title: "Berenang renang ke tepian",
-                description: "Besok saya mau berenang renang ke tepian",
-                isDone: false
-            },
-            {
-                id: 2,
-                title: "Mendayung melewati dua tiga pulau",
-                description: "Saya ingin melewati dua tiga pulau dengan sekali mendayung",
-                isDone: true
-            }
-        ]
+        result: result.length,
+        data: result
     });
 });
 exports.getAllTodosHandler = getAllTodosHandler;
 const craeteTodoHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield connect_1.default
+        .insertInto('todo')
+        .values({
+        title: request.body.title,
+        description: request.body.description,
+        is_done: request.body.is_done,
+        slug: 'gfsdfbvddfvdb'
+    })
+        .returningAll()
+        .executeTakeFirst();
     reply
         .code(201)
         .header('Content-Type', 'application/json; charset=utf-8')
         .send({
         status: "success",
-        data: {
-            id: 3,
-            title: request.body.title,
-            description: request.body.description,
-            isDone: request.body.isDone
-        }
+        data: result
     });
 });
 exports.craeteTodoHandler = craeteTodoHandler;
 const getSingleTodoHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
     const id = request.params.id;
-    if (id === 1) {
-        reply
-            .code(200)
-            .header('Content-Type', 'application/json; charset=utf-8')
-            .send({
-            status: "success",
-            data: {
-                id: 1,
-                title: "Berenang renang ke tepian",
-                description: "Besok saya mau berenang renang ke tepian",
-                isDone: false
-            }
-        });
-    }
-    else if (id === 2) {
-        reply
-            .code(200)
-            .header('Content-Type', 'application/json; charset=utf-8')
-            .send({
-            status: "success",
-            data: {
-                id: 2,
-                title: "Mendayung melewati dua tiga pulau",
-                description: "Saya ingin melewati dua tiga pulau dengan sekali mendayung",
-                isDone: true
-            }
-        });
-    }
-});
-exports.getSingleTodoHandler = getSingleTodoHandler;
-const updateSingleTodoHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = request.params.id;
+    const result = yield connect_1.default
+        .selectFrom('todo')
+        .selectAll()
+        .where('id', '=', id)
+        .executeTakeFirst();
     reply
         .code(200)
         .header('Content-Type', 'application/json; charset=utf-8')
         .send({
         status: "success",
-        data: {
-            id: 2,
-            title: "Mendayung melewati 2 tiga pulau",
-            description: "Saya ingin melewati dua tiga pulau dengan sekali mendayung",
-            isDone: false
-        }
+        data: result
+    });
+});
+exports.getSingleTodoHandler = getSingleTodoHandler;
+const updateSingleTodoHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = request.params.id;
+    console.log(id);
+    const result = yield connect_1.default
+        .updateTable('todo')
+        .set(request.body)
+        .where('id', '=', id)
+        .returningAll()
+        .executeTakeFirst();
+    reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({
+        status: "success",
+        data: result
     });
 });
 exports.updateSingleTodoHandler = updateSingleTodoHandler;
 const deleteSingleTodoHandler = (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
     const id = request.params.id;
+    yield connect_1.default
+        .deleteFrom('todo')
+        .where('id', '=', id)
+        .execute();
     reply
         .code(204);
 });
