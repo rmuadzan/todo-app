dfc472da47f7be36d7e6b6ca11a027c03f439c51
Rafli Muadzan <raflimuadzan2@gmail.com>
2023-02-16 10:35:47 +0800

    add bcrypt funcionality to store and validate user password in database

--- dist/services/userService.js
+++ dist/services/userService.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserInfoByEmail = exports.getUserPasswordByEmail = exports.createUser = void 0;
+exports.getUserInfoByEmail = exports.createUser = void 0;
 const connect_1 = __importDefault(require("../db/connect"));
 const helpers_1 = require("../helpers");
 function createUser(email, password, fullname) {
@@ -31,17 +31,6 @@ function createUser(email, password, fullname) {
     });
 }
 exports.createUser = createUser;
-function getUserPasswordByEmail(email) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const result = yield connect_1.default
-            .selectFrom('person')
-            .select(['password'])
-            .where('email', '=', email)
-            .executeTakeFirst();
-        return result;
-    });
-}
-exports.getUserPasswordByEmail = getUserPasswordByEmail;
 function getUserInfoByEmail(email) {
     return __awaiter(this, void 0, void 0, function* () {
         const result = yield connect_1.default
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,20 @@
 import slugify from "slugify"
+import bcrypt from "bcrypt"
 
 export function convertStringToSlugFormat(data: string): string {
   const slug = slugify(data, {replacement: '-', trim: true, lower: true})
 
   return slug
+}
+
+export async function generateHashedPassword(password: string) {
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  return hashedPassword
+}
+
+export async function comparePassword(candidatePassword: string, actualPassword: string): Promise<boolean> {
+  const match = await bcrypt.compare(candidatePassword, actualPassword)
+
+  return match
 }
